3ae6046233c127dd4f0e6734d31f85f34a30cfb6
Valentina <valentinaolmosoo@gmail.com>
2025-07-02 17:45:52 -0300

    Add files via upload

---
+++ ProgramacionGUIAej1/ProgramacionGUIAej1/Class/Citizen.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramacionGUIAej1
+{
+    public class Citizen
+    {
+        private string fullName { get; set; }
+        private string dni { get; set; }
+        private int age { get; set; }
+
+        public Citizen(string fullName, string dni, int age)
+        {
+            this.fullName = fullName;
+            this.dni = dni;
+            this.age = age;
+        }
+
+        public void Greet()
+        {
+            if (age >= 0)
+            {
+                Console.WriteLine($"{fullName}: Hello! i am {age} years old.");
+            }
+            else
+            {
+                Console.WriteLine("(ERROR): Invalid age");
+            }
+        }
+
+        public void IsAdult()
+        {
+            if (age >= 18)
+            {
+                Console.WriteLine("The citizen is an adult.");
+            }
+            else
+            {
+                Console.WriteLine("The citizen is a minor");
+            }
+        }
+    }
+}
---
+++ ProgramacionGUIAej1/ProgramacionGUIAej1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProgramacionGUIAej1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Citizen newCitizen = new Citizen("Valentina Olmos", "46206232", 20);
+            newCitizen.Greet();
+            newCitizen.IsAdult();
+        }
+    }
+}
